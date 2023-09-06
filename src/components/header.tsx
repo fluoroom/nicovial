f02836d2cd4f7838fc7f8056eeb479842729f52e
nico <nico.vial.giudici@gmail.com>
2023-09-06 19:19:45 -0300

    first

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         <ul>
           <li><a href="https://www.instagram.com/rytratt/" rel='noreferrer' target="_blank"><img src="/images/insta.png" alt="" /></a></li>
           <li><a href="https://www.facebook.com/rytratt" rel='noreferrer' target="_blank"><img src="/images/fb.png" alt="" /></a></li>
-          <li><a href="https://www.behance.net/nicovial" rel='noreferrer' target="_blank"><img src="../images/beh.png" alt="" /></a></li>
+          <li><a href="https://www.behance.net/nicovial"  rel='noreferrer' target="_blank"><img src="../images/beh.png" alt="" /></a></li>
         </ul>
       </nav>
     </header>
