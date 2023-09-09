9564c9df004aa2fc011123b512ab1aa7b49aa609
nico <nico.vial.giudici@gmail.com>
2023-09-09 16:34:21 -0300

    shuffle array client

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,7 @@ export const  metadata = {
 }
 
 const Page = async () =>{
-  function shuffleArray(array) {
-    const newArray = [...array]; // Create a shallow copy of the input array to avoid modifying the original array.
-    for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-    }
-    return newArray;
-  }
-    const albums = shuffleArray(await getAlbums()); 
+    const albums = await getAlbums(); 
 
     return <Home albums={albums}/>
 }
